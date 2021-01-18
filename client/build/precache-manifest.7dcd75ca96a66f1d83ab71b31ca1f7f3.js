665cf779bd4ed72d48ea1837dfdf96ecbcdfd189
Ritik Pandey <rp18.pandey@gmail.com>
2021-01-18 19:13:56 +0530

    new build

---
+++ client/build/precache-manifest.7dcd75ca96a66f1d83ab71b31ca1f7f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a01c66cb22b40c7cc3957ff764d288d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51028542fc0a68e0072d",
+    "url": "/static/css/main.2b4816a4.chunk.css"
+  },
+  {
+    "revision": "ae1ccd91e53d639a64bc",
+    "url": "/static/js/2.0b45fbb6.chunk.js"
+  },
+  {
+    "revision": "dfff224622d7a184e2c5746c5298c29d",
+    "url": "/static/js/2.0b45fbb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51028542fc0a68e0072d",
+    "url": "/static/js/main.86fc5fbc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "99b2a16fab3e1f1ee1d0724931922e35",
+    "url": "/static/media/loader2.99b2a16f.gif"
+  }
+]);
